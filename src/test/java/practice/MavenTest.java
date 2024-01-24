426d505789199f1211af3c3de2fc3b5884907f28
suyas <suyas@Suyasha_Masand>
2024-01-24 17:11:59 +0530

    commit test 2

---
+++ src/test/java/practice/MavenTest.java
@@ -0,0 +1,24 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class MavenTest {
+	
+
+public class PracticeTest {
+	@Test(groups = {"smoke","regression"})
+	public void practiceOne() {
+		System.out.println("--testScript7--");
+	}
+	@Test(groups = "regression")
+	public void practiceTwo() {
+		System.out.println("--testScript8--");
+	}
+	@Test(groups = "regression")
+	public void practiceThree() {
+		System.out.println("--testScript9--");
+	}
+}
+
+
+}
