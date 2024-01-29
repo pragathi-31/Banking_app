46277df6752c3cb83d32e7c1a84f5a9f8611b08e
Akshatahb <akshatahbelagali1600@gmail.com>
2024-01-29 13:47:43 +0530

    change 1

--- src/test/java/practice/MavenTest.java
+++ src/test/java/practice/MavenTest.java
@@ -10,14 +10,17 @@ public class PracticeTest {
 	@Test(groups = {"smoke","regression"})
 	public void practiceOne() {
 		System.out.println("--testScript7--");
+		
 	}
 	@Test(groups = "regression")
 	public void practiceTwo() {
 		System.out.println("--testScript8--");
+		
 	}
 	@Test(groups = "regression")
 	public void practiceThree() {
 		System.out.println("--testScript9--");
+		
 		
 	}
 }
